4f3bcaa08ba7753e21a04d685f7bca0702336120
Sean Durham <sean@sdurham.net>
2018-03-12 15:57:03 -0700

    WIP

--- packages/vanilla/scripts/build.js
+++ packages/vanilla/scripts/build.js
@@ -4,18 +4,24 @@ const string = require('rollup-plugin-string');
 const collectSass = require('rollup-plugin-collect-sass');
 const svg = require('rollup-plugin-svg');
 const rootImport = require('rollup-plugin-root-import');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const json = require('rollup-plugin-json');
 
 const inputOptions = {
   input: 'src/index.js',
   plugins: [
+    resolve(),
+    commonjs(),
     rootImport({
       root: `${__dirname}/src`,
       useEntry: 'prepend',
       extensions: '.js'
     }),
     babel({
-      exclude: ['node_modules/**', '**/*.scss', '**/*.html', '**/*.svg']
+      exclude: ['node_modules/**', '**/*.scss', '**/*.html', '**/*.svg', '**/*.json']
     }),
+    json(),
     collectSass({
       include: '**/*.scss',
       extract: 'lib/hig.css',
@@ -25,19 +31,31 @@ const inputOptions = {
       include: '**/*.html'
     }),
     svg()
-  ],
+  ]
+};
+
+const esModulesOutputOptions = {
+  name: 'HigVanilla',
+  file: 'lib/hig.es.js',
+  format: 'es',
   external: ['hig-interface', 'mustache', 'i18next']
 };
 
-const outputOptions = {
-  name: 'HigVanilla',
+const browserFriendlyOutputOptions = {
+  name: 'Hig',
   file: 'lib/hig.js',
-  format: 'es'
+  format: 'umd'
 };
 
 async function build() {
-  const bundle = await rollup.rollup(inputOptions);
-  bundle.write(outputOptions);
+  let bundle;
+  try {
+    bundle = await rollup.rollup(inputOptions);
+    // bundle.write(esModulesOutputOptions);
+    bundle.write(browserFriendlyOutputOptions);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 build();
